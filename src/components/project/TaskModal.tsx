01ac88b159603d85eaba0cfb2628c87e387bfde0
EvgenKlo <klokovevgen@gmail.com>
2023-10-03 17:19:26 +0500

    feat: task modal adaptive

--- src/components/project/TaskModal.tsx
+++ src/components/project/TaskModal.tsx
@@ -29,8 +29,8 @@ const style = {
   minWidth: 320,
   bgcolor: 'background.paper',
   boxShadow: 24,
-  p: 4,
   borderRadius: 5,
+  p: 4,
   boxSizing: 'border-box',
 };
 
@@ -155,165 +155,167 @@ const TaskModal: React.FC<{
             </>
           )}
         </Box>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
-        >
-          Name:{' '}
-          {edit ? (
-            <TextField
-              fullWidth
-              multiline
-              variant="standard"
-              type="input"
-              value={taskUpdate.name}
-              onChange={(e) => {
-                setTaskUpdate({ ...taskUpdate, name: e.target.value });
-              }}
-              sx={{ marginLeft: 1 }}
-            />
-          ) : (
-            task.name
-          )}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
-        >
-          Description:{' '}
-          {edit ? (
-            <TextField
-              fullWidth
-              multiline
-              rows={5}
-              variant="standard"
-              type="input"
-              value={taskUpdate.description}
-              onChange={(e) => {
-                setTaskUpdate({ ...taskUpdate, description: e.target.value });
-              }}
-              sx={{ marginLeft: 1 }}
-            />
-          ) : (
-            task.description
-          )}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
-        >
-          {`Date of creation: ${new Date(task.dateCreate).toLocaleString()}`}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1 }}
-        >
-          {`Time at work: ${msToTime(Date.now() - task.dateCreate)}`}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
-        >
-          Priority:{' '}
-          {edit ? (
-            <FormControl required>
-              <Select
-                value={taskUpdate.priority}
+        <Box sx={{ maxHeight: '80vh', overflow: 'auto' }}>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
+          >
+            Name:{' '}
+            {edit ? (
+              <TextField
+                fullWidth
+                multiline
+                variant="standard"
+                type="input"
+                value={taskUpdate.name}
                 onChange={(e) => {
-                  setTaskUpdate({ ...taskUpdate, priority: e.target.value as Priority });
+                  setTaskUpdate({ ...taskUpdate, name: e.target.value });
                 }}
                 sx={{ marginLeft: 1 }}
+              />
+            ) : (
+              task.name
+            )}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
+          >
+            Description:{' '}
+            {edit ? (
+              <TextField
+                fullWidth
+                multiline
+                rows={5}
                 variant="standard"
-              >
-                {['low', 'medium', 'high'].map((priority) => {
-                  return (
-                    <MenuItem
-                      key={priority}
-                      value={priority}
-                    >
-                      {priority}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          ) : (
-            task.priority
-          )}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
-        >
-          Status:{' '}
-          {edit ? (
-            <FormControl required>
-              <Select
-                value={taskUpdate.status}
+                type="input"
+                value={taskUpdate.description}
                 onChange={(e) => {
-                  setTaskUpdate({ ...taskUpdate, status: e.target.value as Status });
+                  setTaskUpdate({ ...taskUpdate, description: e.target.value });
                 }}
                 sx={{ marginLeft: 1 }}
-                variant="standard"
-              >
-                {['Queue', 'Development', 'Done'].map((status) => {
-                  return (
-                    <MenuItem
-                      key={status}
-                      value={status}
-                    >
-                      {status}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </FormControl>
-          ) : (
-            task.status
+              />
+            ) : (
+              task.description
+            )}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
+          >
+            {`Date of creation: ${new Date(task.dateCreate).toLocaleString()}`}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1 }}
+          >
+            {`Time at work: ${msToTime(Date.now() - task.dateCreate)}`}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
+          >
+            Priority:{' '}
+            {edit ? (
+              <FormControl required>
+                <Select
+                  value={taskUpdate.priority}
+                  onChange={(e) => {
+                    setTaskUpdate({ ...taskUpdate, priority: e.target.value as Priority });
+                  }}
+                  sx={{ marginLeft: 1 }}
+                  variant="standard"
+                >
+                  {['low', 'medium', 'high'].map((priority) => {
+                    return (
+                      <MenuItem
+                        key={priority}
+                        value={priority}
+                      >
+                        {priority}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            ) : (
+              task.priority
+            )}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: 1, display: 'flex', overflow: 'hidden' }}
+          >
+            Status:{' '}
+            {edit ? (
+              <FormControl required>
+                <Select
+                  value={taskUpdate.status}
+                  onChange={(e) => {
+                    setTaskUpdate({ ...taskUpdate, status: e.target.value as Status });
+                  }}
+                  sx={{ marginLeft: 1 }}
+                  variant="standard"
+                >
+                  {['Queue', 'Development', 'Done'].map((status) => {
+                    return (
+                      <MenuItem
+                        key={status}
+                        value={status}
+                      >
+                        {status}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            ) : (
+              task.status
+            )}
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ margin: 0.5 }}
+            onClick={() => setSubtasksModal(true)}
+          >
+            Subtasks{' '}
+            {task.subtasks
+              ? task.subtasks?.length
+                ? task.subtasks?.filter((item) => item.complete === false).length
+                : 0
+              : 0}{' '}
+            / {task.subtasks ? task.subtasks?.length : 0}
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 0.5 }}
+            onClick={() => setCommentsOpen(!commentsOpen)}
+          >
+            Comments {comments ? (comments.length ? comments.length : 0) : 0}
+          </Button>
+          {commentsOpen && (
+            <CommentContent
+              task={task}
+              taskComments={task.comments}
+            />
           )}
-        </Typography>
-        <Button
-          variant="contained"
-          sx={{ margin: 0.5 }}
-          onClick={() => setSubtasksModal(true)}
-        >
-          Subtasks{' '}
-          {task.subtasks
-            ? task.subtasks?.length
-              ? task.subtasks?.filter((item) => item.complete === false).length
-              : 0
-            : 0}{' '}
-          / {task.subtasks ? task.subtasks?.length : 0}
-        </Button>
-        <Button
-          variant="contained"
-          sx={{ margin: 0.5 }}
-          onClick={() => setCommentsOpen(!commentsOpen)}
-        >
-          Comments {comments ? (comments.length ? comments.length : 0) : 0}
-        </Button>
-        {commentsOpen && (
-          <CommentContent
-            task={task}
-            taskComments={task.comments}
+          <DeleteTaskModal
+            open={deleteModal}
+            setOpen={setDeleteModal}
+            deleteTask={deleteTask}
           />
-        )}
-        <DeleteTaskModal
-          open={deleteModal}
-          setOpen={setDeleteModal}
-          deleteTask={deleteTask}
-        />
-        <SubtaskModal
-          modalOpen={subtasksModal}
-          setModalOpen={setSubtasksModal}
-          task={task}
-        />
+          <SubtaskModal
+            modalOpen={subtasksModal}
+            setModalOpen={setSubtasksModal}
+            task={task}
+          />
+        </Box>
       </Box>
     </Modal>
   );
